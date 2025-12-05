dd2d6de2faf85d5b6a827ca1fd405e9f07fc42bb
palmiery <rpscti@hotmail.com>
2025-12-05 07:39:12 -0300

    Bilioteca aloca_esquadra.h

---
+++ include/aloca_esquadra.h
@@ -0,0 +1,9 @@
+#ifndef ALOCA_ESQUADRA_H_INCLUDED
+#define ALOCA_ESQUADRA_H_INCLUDED
+
+#define TAM 15
+
+// Apenas a assinatura da função. Sem o código do corpo { ... }
+int aloca_esquadra(int matriz[TAM][TAM]);
+
+#endif // ALOCA_ESQUADRA_H_INCLUDED
