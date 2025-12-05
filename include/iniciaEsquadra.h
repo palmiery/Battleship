f99685548a23b850129cbf6674c5c25f7416f8aa
palmiery <rpscti@hotmail.com>
2025-12-05 09:04:09 -0300

    Biblioteca iniciaEsquadra.h

---
+++ include/iniciaEsquadra.h
@@ -0,0 +1,12 @@
+#ifndef INICIAESQUADRA_H_INCLUDED
+#define INICIAESQUADRA_H_INCLUDED
+
+// Garante a definição de TAM
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo Corrigido: Agora recebe a matriz que deve ser inicializada
+void iniciaEsquadra(int matriz[TAM][TAM]);
+
+#endif // INICIAESQUADRA_H_INCLUDED
