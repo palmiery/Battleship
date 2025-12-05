f2d248e70e1463f3817171999ae754606b3b61a3
palmiery <rpscti@hotmail.com>
2025-12-05 09:13:32 -0300

    Biblioteca tiromanual.h

---
+++ include/tiromanual.h
@@ -0,0 +1,12 @@
+#ifndef TIROMANUAL_H_INCLUDED
+#define TIROMANUAL_H_INCLUDED
+
+// Garante a definição de TAM
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo da função
+int tiromanual(int matriz[TAM][TAM], int matriz2[TAM][TAM]);
+
+#endif // TIROMANUAL_H_INCLUDED
