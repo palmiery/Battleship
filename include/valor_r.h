3fd0d9c70916a5fb38848425da56750290a72d9a
palmiery <rpscti@hotmail.com>
2025-12-05 09:15:39 -0300

    Biblioteca valor_r.h

---
+++ include/valor_r.h
@@ -0,0 +1,13 @@
+#ifndef VALOR_R_H_INCLUDED
+#define VALOR_R_H_INCLUDED
+
+// Garante que TAM esteja definido
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo da função
+// Gera valores aleatórios para linha, coluna e posição
+void valor_r(int *linha, int *coluna, int *pos);
+
+#endif // VALOR_R_H_INCLUDED
