a916638c7884151a179da6e14493387d078f637c
palmiery <rpscti@hotmail.com>
2025-12-05 09:09:47 -0300

    Biblioteca LerLinColuna.h

---
+++ include/LerLinColuna.h
@@ -0,0 +1,12 @@
+#ifndef LERLINCOLUNA_H_INCLUDED
+#define LERLINCOLUNA_H_INCLUDED
+
+// Garante que TAM esteja definido
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo da função
+void LerLinColuna(int *linha, int *coluna, int *pos);
+
+#endif // LERLINCOLUNA_H_INCLUDED
