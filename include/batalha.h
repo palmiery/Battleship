11119efbcb2d8f6ce34f98ae6517bfb0fdd18c3a
palmiery <rpscti@hotmail.com>
2025-12-05 08:59:49 -0300

    Biblioteca batalha.h

---
+++ include/batalha.h
@@ -0,0 +1,13 @@
+#ifndef BATALHA_H_INCLUDED
+#define BATALHA_H_INCLUDED
+
+// Garante que TAM esteja definido
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo da função
+// Recebe duas matrizes: a do jogador e a do computador/inimigo
+int batalha(int matriz[TAM][TAM], int matriz2[TAM][TAM]);
+
+#endif // BATALHA_H_INCLUDED
