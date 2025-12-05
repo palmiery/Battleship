b9b87cb546bbb7a40ca3e3900358a43ffdcd0b5a
palmiery <rpscti@hotmail.com>
2025-12-05 09:14:38 -0300

    Biblioteca valor_c.h

---
+++ include/valor_c.h
@@ -0,0 +1,13 @@
+#ifndef VALOR_C_H_INCLUDED
+#define VALOR_C_H_INCLUDED
+
+// Garante a definição de TAM
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo da função
+// Recebe um ponteiro para a letra digitada e um ponteiro para salvar o valor numérico
+void valor_c(char *letra, int *valor);
+
+#endif // VALOR_C_H_INCLUDED
