9b10279c54d908d961ceee62c9a7a7f359101725
palmiery <rpscti@hotmail.com>
2025-12-05 09:05:19 -0300

    Biblioteca iniciaTabuleiro.h

---
+++ include/iniciaTabuleiro.h
@@ -0,0 +1,12 @@
+#ifndef INICIATABULEIRO_H_INCLUDED
+#define INICIATABULEIRO_H_INCLUDED
+
+// Define TAM se ainda não foi definido
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Apenas o protótipo da função
+void iniciatabuleiro(int matriz[TAM][TAM]);
+
+#endif // INICIATABULEIRO_H_INCLUDED
