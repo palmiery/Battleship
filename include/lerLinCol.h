9b95adfa7db370c11c446e83f089f46138bfb1e9
palmiery <rpscti@hotmail.com>
2025-12-05 09:07:13 -0300

    Biblioteca lerLinCol.h

---
+++ include/lerLinCol.h
@@ -0,0 +1,12 @@
+#ifndef LERLINCOL_H_INCLUDED
+#define LERLINCOL_H_INCLUDED
+
+// Garante que TAM esteja definido apenas uma vez
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo da função
+void lerLinCol(int *linha, int *coluna);
+
+#endif // LERLINCOL_H_INCLUDED
