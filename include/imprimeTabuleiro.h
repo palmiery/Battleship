4d24d76055c582167ce55385bb1e404c6af520ad
palmiery <rpscti@hotmail.com>
2025-12-05 09:02:54 -0300

    Biblioteca imprimeTabuleiro.h

---
+++ include/imprimeTabuleiro.h
@@ -0,0 +1,12 @@
+#ifndef IMPRIMETABULEIRO_H_INCLUDED
+#define IMPRIMETABULEIRO_H_INCLUDED
+
+// Proteção para não redefinir TAM se ele já existir
+#ifndef TAM
+    #define TAM 15
+#endif
+
+// Protótipo da função
+void imprimeTabuleiro(int matriz[TAM][TAM]);
+
+#endif // IMPRIMETABULEIRO_H_INCLUDED
